b71659bacc735b872c2488c63fe46cfa44900211
java3011 <72117272+java3011@users.noreply.github.com>
2023-06-28 19:29:52 -0400

    Add files via upload

---
+++ front-end/src/app/pages/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { LoginService } from 'src/app/services/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit{
+
+ 
+  user = {
+    Email: '',
+    userPassword: ''
+  }
+
+  constructor(private login: LoginService, private router: Router) {}
+  
+  ngOnInit(): void {
+    
+  }
+
+  logIn(){
+    console.log(this.user);
+    this.login.singin(this.user).subscribe( (res:any) => {
+      console.log(res);
+      localStorage.setItem('token', res.token);
+      this.router.navigate(['private']);
+    })
+  }
+
+}
